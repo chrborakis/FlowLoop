fecfecec2d88105a5dfaf5d9bd32516ed1c97d5f
chrborakis <christos.borakis99@gmail.com>
2023-12-16 15:41:00 +0200

    private chat bug fix

--- apps/chats/models.py
+++ apps/chats/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.forms import ValidationError
 from django.utils.timezone import now
-
+from django.utils import timezone
 from apps.users.models import Friends
 
 class PrivateChat(models.Model):
@@ -9,14 +9,20 @@ class PrivateChat(models.Model):
     sender     = models.ForeignKey(Friends, related_name='sender',on_delete = models.CASCADE)
     receiver   = models.ForeignKey(Friends, related_name='receiver',on_delete=models.CASCADE)
     message    = models.TextField(null=False,blank=False)
-    date       = models.DateField(default=now, editable=False, blank=True, null=True)
+    date       = models.DateTimeField(default=timezone.now, editable=False, blank=True, null=True)
 
     def clean(self):
         if self.sender == self.receiver:
             raise ValidationError("Receiver cannot be the sender!")
+        
+        if self.sender.person != self.receiver.friend:
+            raise ValidationError("Sender and Receiver must be friends.")
+        super().clean()
+
 
     def __str__(self):
-        return f'[{self.sender.person}->{self.receiver.friend}] {self.message}'
+        return f'[{self.sender.person}->{self.receiver.person}] {self.message}'
 
     class Meta:
         db_table = 'private_chat'
+
